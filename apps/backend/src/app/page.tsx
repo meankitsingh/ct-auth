6feb4d42d89ff43e04cf6e5fba3bce8dc7d9e0c6
Ankit Singh <ankit@pixlevents.com>
2026-05-23 09:28:50 +0530

    update

--- apps/backend/src/app/page.tsx
+++ apps/backend/src/app/page.tsx
@@ -4,12 +4,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      Welcome to Stack Auth&apos;s API endpoint.<br />
-      <br />
-      Were you looking for <Link href="https://accounts.cognitiontree.com">Stack&apos;s dashboard</Link> instead?<br />
-      <br />
-      You can also return to <Link href="https://stack-auth.com">https://stack-auth.com</Link>.<br />
-      <br />
+      Welcome to CognitionTree Shiel API endpoint.<br />
       <Link href="/api/v1">API v1</Link><br />
       {getNodeEnvironment() === "development" && (
         <>
